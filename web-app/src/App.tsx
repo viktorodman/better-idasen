49331fb8294067545b63f942fb42966bfddb346a
Viktor Ödman <vo222dq@student.lnu.se>
2022-07-23 19:21:03 +0200

    updating

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -6,8 +6,11 @@ const App = () => {
   const [device, setDevice] = useState<any>(null);
   const [server, setServer] = useState<any>(null);
   const [service, setService] = useState<any>(null);
+  const [currentPos, setCurrentPos] = useState<any>(null);
+
   const serviceID = '99fa0001-338a-1024-8a49-009c0215f78a'
   const positionServiceID = '99fa0020-338a-1024-8a49-009c0215f78a'
+  const positionCharID = '99fa0021-338a-1024-8a49-009c0215f78a'
   const charID = '99fa0002-338a-1024-8a49-009c0215f78a'
 
   /* useEffect(() => {
@@ -33,21 +36,98 @@ const App = () => {
       acceptAllDevices: true,
     }).catch((error: any) => { console.log(error.message) })
 
-    setDevice(btDevice)
+    /* const gattServer = await btDevice.gatt.connect()
+    console.log("Gatt Server: ", gattServer)
+    const primaryService = await gattServer.getPrimaryService(serviceID)
+    console.log("Primary Service ", primaryService) */
+    /* const test = navigatorObject.bluetooth.requestDevice(
+      { 
+        optionalServices: [serviceID, positionServiceID],
+        acceptAllDevices: true, 
+    })
+    .then((device:any) => {
+  // Human-readable name of the device.
+  console.log("In first then")
+      console.log(device.name);
 
-    btDevice.addEventListener('gattserverdisconnected', () => console.log("first"))
+  // Attempts to connect to remote GATT Server.
+    return device.gatt.connect();
+    })
+    .then((server:any) => { 
+      console.log("In second then")
+      console.log(server)
+    })
+    .catch((error:any) => { console.error(error); }); */
 
-    console.log(btDevice)
+    if (btDevice) {
+      
+      setDevice(btDevice)
+  
+      btDevice.addEventListener('gattserverdisconnected', () => console.log("first"))
+  
+      console.log(btDevice)
+    }
+
   }
 
   const connect = async () => {
     if (!device) throw "Device is not connected!"
 
-    const gattServer = await device.gatt.connect()
-    console.log(gattServer)
+    console.log("in connect")
 
-    const primaryService = await gattServer.getPrimaryService(serviceID)
-    console.log(primaryService)
+    try {
+      console.log(device.gatt)
+      const gattServer = await device.gatt.connect()
+      setServer(gattServer)
+      console.log("Gatt Server: ", gattServer)
+      const primaryService = await gattServer.getPrimaryService(serviceID)
+      setService(primaryService)
+      console.log("Primary Service ", primaryService)
+    } catch (error) {
+      console.log("Error")
+      console.log(error)
+    }
+
+    /* const primaryService = await gattServer.getPrimaryService(serviceID)
+    console.log("Primary Service " + primaryService) */
+  }
+
+  const getCurrentPos = async () => {
+    const posService = await server.getPrimaryService(positionServiceID);
+    const char = await posService.getCharacteristic(positionCharID);
+    const value = await char.readValue()
+    const readPosBuffer = await value.buffer
+    console.log(readPosBuffer)
+    setCurrentPos(new Uint16Array(readPosBuffer)[0])
+  }
+
+  const moveUp = async () => {
+    console.log("Going up")
+    const char = await service.getCharacteristic(charID)
+    await char.writeValue(hexStrToArray('4700'))
+    await getCurrentPos()
+  }
+
+  const moveDown = async () => {
+    console.log("Going down")
+    const char = await service.getCharacteristic(charID)
+    await char.writeValue(hexStrToArray('4600'))
+    await getCurrentPos()
+  }
+
+  const stop = async () => {
+    console.log("STOOOOP")
+    const char = await service.getCharacteristic(charID)
+    await char.writeValue(hexStrToArray('FF00'))
+    await getCurrentPos()
+  }
+
+  const hexStrToArray = (hexString:any) => {
+    let decimals = []
+    for (let i = 0; i < hexString.length; i += 2) {
+      decimals.push(parseInt(hexString.substr(i, 2), 16))
+    }
+    return new Uint8Array(decimals)
   }
 
   return (
@@ -56,7 +136,20 @@ const App = () => {
         <h1>BETTER IDÅSEN</h1>
         <button onClick={async () => { await findDevice() }} >Find Device</button>
         <h1>Found device: {device === null ? "No device connected" : device.name}</h1>
-        {device !== null ? <button onClick={async () => await connect()} >Connect</button> : null}
+        <p>Gatt Server id: {server !== null ? server.device.id : ""}</p>
+        <p>Gatt Service uuid: {service !== null ? service.uuid : ""}</p>
+        <p>Current Pos: {currentPos !== null ? currentPos : "No pos set"}</p>
+        {device !== null ? 
+          (
+            <div>
+              <button onClick={async () => await connect()} >Connect</button>
+              <button onClick={async () => await getCurrentPos()}>Get Current Pos</button>
+              <button onClick={async () => await moveUp()}>Up</button>
+              <button onClick={async () => await moveDown()}>Down</button>
+              <button onClick={async () => await stop()}>Stop</button>
+            </div>
+          ) : null
+          }
       </header>
     </div>
   );
