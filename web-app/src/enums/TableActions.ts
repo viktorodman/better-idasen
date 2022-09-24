7e605458034f5e2d0b9c66822d81007316766293
viktorodman <odman94@gmail.com>
2022-09-24 18:14:27 +0200

    updating

--- web-app/src/enums/TableActions.ts
+++
@@ -1,5 +0,0 @@
-export enum TableActions {
-    DOWN = '4600',
-    UP = '4700',
-    STOP = 'FF00'
-}
--- web-app/src/helpers/general.ts
+++
@@ -1,7 +0,0 @@
-export const hexStrToArray = (hexString: any) => {
-    let decimals = []
-    for (let i = 0; i < hexString.length; i += 2) {
-        decimals.push(parseInt(hexString.substr(i, 2), 16))
-    }
-    return new Uint8Array(decimals)
-}
